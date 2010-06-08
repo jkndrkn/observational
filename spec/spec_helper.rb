cadfed10909f6e22a7716f54244ab38ede499db5
Ben Mabey <ben@benmabey.com>
2010-06-08 10:32:21 -0600

    extracts AR env from spec_helper to reduce clutter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,44 +10,4 @@ Spec::Runner.configure do |config|
   config.mock_with :mocha
 end
 
-ActiveRecord::Base.configurations = {'sqlite3' => {:adapter => 'sqlite3', :database => ':memory:'}}
-ActiveRecord::Base.establish_connection('sqlite3')
-
-ActiveRecord::Base.logger = Logger.new(STDERR)
-ActiveRecord::Base.logger.level = Logger::WARN
-
-ActiveRecord::Schema.define(:version => 0) do
-  create_table :users do |t|
-    t.string  :email,    :default => ''
-  end
-
-  create_table :messages do |t|
-    t.integer :creator_id
-    t.string  :email,    :default => ''
-  end
-end
-
-class User < ActiveRecord::Base
-end
-
-class Message < ActiveRecord::Base
-  belongs_to :creator, :class_name => "User"
-end
-
-class Notifier
-  observes :user, :invokes => :deliver_new_user, :on => :after_create
-
-  def self.deliver_new_user(user)
-  end
-end
-
-class Creditor
-  observes :message, :invokes => :use_credits, :with => :creator, :on => :create
-
-  def self.use_credits(user)
-  end
-end
-
-require 'observational/active_record_observers'
-ActiveRecord::Base.send(:include, Observational::ActiveRecordObservers)
-
+require 'active_record_env'
